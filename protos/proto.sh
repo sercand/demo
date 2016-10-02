a89e1f231e59d67b46a01c10fa88e6dd0a3b6b32
Sercan Degirmenci <degirmencisercan@gmail.com>
2016-10-03 00:17:01 +0300

    use grpc-gateway

--- protos/proto.sh
+++ protos/proto.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-protoc --proto_path=. \
+GOOGLE_APIPATH=../vendor/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis
+
+protoc --proto_path=.:$GOOGLE_APIPATH \
     --gofast_out=plugins=grpc:../src/feedpb \
-    --
+    --grpc-gateway_out=logtostderr=true:../src/feedpb \
     ./*.proto
