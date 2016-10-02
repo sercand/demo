96b5cf1fad74e1173bdd01dcfeed68a3b464ec77
Sercan Degirmenci <degirmencisercan@gmail.com>
2016-10-03 00:18:06 +0300

    return two dummy item

--- src/feed-node/server.js
+++ src/feed-node/server.js
@@ -7,12 +7,13 @@ function newItem() {
 }
 
 function getItems(call, callback) {
-    callback(null, { items: [] });
+    console.log("GET", call.request);
+    callback(null, {items: [{title:"ok"},{title:"ok1"}]});
 }
 
 function main() {
     var server = new grpc.Server();
-    server.addProtoService(feed_proto.FeedProvider.service, { get: getItems });
+    server.addProtoService(feed_proto.FeedProvider.service, {get: getItems});
     server.bind('0.0.0.0:3020', grpc.ServerCredentials.createInsecure());
     server.start();
 }
